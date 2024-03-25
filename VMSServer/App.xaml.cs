e6836a5d5662ecaa3851d0f8a2a02898bfcc26ba
AngelPineda1 <201G0276@rcarbonifera.tecnm.mx>
2024-03-25 11:21:49 -0600

    Agregar archivos de proyecto.

---
+++ VMSServer/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace VMSServer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
